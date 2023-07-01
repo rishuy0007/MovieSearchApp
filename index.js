45397f17498c55e1091c09ebeb5f43a2abf0d348
Rishabh Yadav <rishuy0007@gmail.com>
2023-07-01 16:54:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH ="https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+const movieBox = document.querySelector("#movie-box");
+
+const getMovies = async (api)=>{
+    const response =  await fetch(api);
+    const data = await response.json();
+    
+    showMovies(data);
+}
+
+   getMovies(APIURL);
+
+    const showMovies = (data)=>{
+
+        movieBox.innerHTML = "";
+        
+        data.results.forEach(
+            (item) => {
+                const imagePath = item.poster_path == null?"images/wed1.jpeg":IMGPATH+item.poster_path;
+            
+            const box = document.createElement("div");
+            box.classList.add("box");
+            
+
+            box.innerHTML = `<img  src="${imagePath}" alt="">
+           
+                <div class="overlay">
+                    <div class="title">
+                        <h1>${item.original_title}</h1>
+                        <span>${item.vote_average}</span>
+
+                    </div>
+                    <h3>Overview:</h3>
+                    <p>
+                    ${item.overview}
+                    </p>
+                    </div>
+            `
+            movieBox.appendChild(box)
+            
+        });
+    }
+
+    document.querySelector("#search").addEventListener("keyup" , (event)=>{
+
+        if(event.target.value !="")
+        {
+            getMovies(SEARCHAPI+event.target.value);
+        }
+        else{
+            getMovies(APIURL);
+        }
+    })
